128ed8887d2577e5aed3fc0178afc94305ec1d65
Ray Plante <raymond.plante@nist.gov>
2016-11-28 17:12:20 -0500

    expose key classes to top level module

---
+++ python/ejsonschema/__init__.py
@@ -0,0 +1,4 @@
+from .validate import ExtValidator
+from .schemaloader import SchemaLoader
+from jsonschema.exceptions import (ValidationError, SchemaError, 
+                                   RefResolutionError)
