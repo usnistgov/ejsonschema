c00e81b73f2dd2695bc7b7d5afa925cc7747471f
Ray Plante <raymond.plante@nist.gov>
2017-02-12 18:00:14 -0500

    added -e to cli script for validating schemas; expose SchemaValidator in root module

--- python/ejsonschema/__init__.py
+++ python/ejsonschema/__init__.py
@@ -1,4 +1,4 @@
-from .validate import ExtValidator
+from .validate import ExtValidator, SchemaValidator
 from .schemaloader import SchemaLoader
 from jsonschema.exceptions import (ValidationError, SchemaError, 
                                    RefResolutionError)
