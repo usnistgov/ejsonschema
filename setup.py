26e57076474a7f96768002711144d25890db3914
RayPlante <raymond.plante@nist.gov>
2023-01-02 10:25:29 -0500

    modernize build infrastructure

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-import glob, os, shutil
-from distutils.core import setup
-from distutils.command.build_py import build_py as _build_py
+import glob, os, shutil, pathlib
+from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py as _build_py
+
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 class build_py(_build_py):
     """Specialization to handle installation of schema files in schemas dir"""
@@ -30,12 +33,29 @@ class build_py(_build_py):
 setup(name='ejsonschema',
       version='0.1',
       description='Enhanced JSON Schema validator (extending jsonschema)',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='Ray Plante',
       author_email='raymond.plante@nist.gov',
       url='https://github.com/usnistgov/ejsonschema',
-      packages=['ejsonschema', 'ejsonschema.cli'],
+      packages=find_packages(where="python"),
       package_dir={'': 'python'},
       scripts=[ 'scripts/validate' ],
-      cmdclass={'build_py': build_py}
-     )
+      cmdclass={'build_py': build_py},
+      python_requires=">=3.7, <4",
+      install_requires=["jsonschema"],
+      project_urls={
+          "Source": "https://github.com/usnistgov/ejsonschema",
+          "Bug Reports": "https://github.com/usnistgov/ejsonschema/issues"
+      },
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
+          "Programming Language :: Python :: 3.8",
+          "Programming Language :: Python :: 3.9",
+          "Programming Language :: Python :: 3.10",
+      ]
+)
 
